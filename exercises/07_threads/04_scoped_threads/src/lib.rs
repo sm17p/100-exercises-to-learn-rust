e3399e43cca7aa021e2c6ee96021b38837c8c69d
Smit <smitp.contact@gmail.com>
2025-07-28 17:48:40 +0530

    🚀: 07_04

--- exercises/07_threads/04_scoped_threads/src/lib.rs
+++ exercises/07_threads/04_scoped_threads/src/lib.rs
@@ -2,8 +2,16 @@
 //  and compute the sum of each half in a separate thread.
 //  Don't perform any heap allocation. Don't leak any memory.
 
+use std::thread;
+
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let mid = v.len() / 2;
+    let (left, right) = v.split_at(mid);
+    thread::scope(|s| {
+        let left = s.spawn(|| left.iter().sum::<i32>());
+        let right = s.spawn(|| right.iter().sum::<i32>());
+        left.join().unwrap() + right.join().unwrap()
+    })
 }
 
 #[cfg(test)]
