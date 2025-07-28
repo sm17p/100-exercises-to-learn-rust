d463966d786ba0b16a3b87c911755b53dfa3249b
Smit <smitp.contact@gmail.com>
2025-07-28 18:20:30 +0530

    🚀: 07_07

--- exercises/07_threads/07_ack/src/lib.rs
+++ exercises/07_threads/07_ack/src/lib.rs
@@ -1,4 +1,7 @@
+use data::{Ticket, TicketDraft};
 use std::sync::mpsc::{Receiver, Sender};
+use store::TicketId;
+
 use crate::store::TicketStore;
 
 pub mod data;
@@ -6,14 +9,20 @@ pub mod store;
 
 // Refer to the tests to understand the expected schema.
 pub enum Command {
-    Insert { todo!() },
-    Get { todo!() }
+    Insert {
+        draft: TicketDraft,
+        response_sender: Sender<TicketId>,
+    },
+    Get {
+        id: TicketId,
+        response_sender: Sender<Option<Ticket>>,
+    },
 }
 
 pub fn launch() -> Sender<Command> {
-    let (sender, receiver) = std::sync::mpsc::channel();
+    let (response_sender, receiver) = std::sync::mpsc::channel();
     std::thread::spawn(move || server(receiver));
-    sender
+    response_sender
 }
 
 // TODO: handle incoming commands as expected.
@@ -21,19 +30,25 @@ pub fn server(receiver: Receiver<Command>) {
     let mut store = TicketStore::new();
     loop {
         match receiver.recv() {
-            Ok(Command::Insert {}) => {
-                todo!()
+            Ok(Command::Insert {
+                draft,
+                response_sender,
+            }) => {
+                let id = store.add_ticket(draft);
+                response_sender.send(id);
             }
             Ok(Command::Get {
-                todo!()
+                id,
+                response_sender,
             }) => {
-                todo!()
+                let ticket = store.get(id);
+                response_sender.send(ticket.cloned());
             }
             Err(_) => {
-                // There are no more senders, so we can safely break
+                // There are no more response_senders, so we can safely break
                 // and shut down the server.
-                break
-            },
+                break;
+            }
         }
     }
 }
